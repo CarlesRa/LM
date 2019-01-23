1233e517fe151824fa28c479e5363b2de19beedf
Carles <https://github.com/CarlesRa>
2019-01-23 17:45:09 +0100

    comence el projecte.

---
+++ ejercicio02JavaScript/index.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow} = require('electron')
+require('electron-reload')(__dirname);
+ 
+let mainWindow
+ 
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadFile('index.html')
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+ 
+app.on('ready', createWindow)
+ 
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+ 
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
